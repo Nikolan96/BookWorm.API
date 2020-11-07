8e369763b041f12a449c92a802661eee94d3c639
Marija Matic <smatic1998@gmail.com>
2020-11-07 17:30:38 +0100

    fixed user interface

--- BookWorm/bookworm/src/app/login-page/login-page.component.ts
+++ BookWorm/bookworm/src/app/login-page/login-page.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import {ErrorStateMatcher} from '@angular/material/core';
-import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { User } from '../user';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
+import { createComponent } from '@angular/compiler/src/core';
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
-
   public user: User = {
+    adress: '',
+    addressId: '',
+    cases: '',
+    dateOfBirth: null,
     email: '',
-    password: ''
+    firstName: '',
+    gender: '',
+    id: '',
+    lastName: '',
+    password: '',
+    userBookNotes: '',
+    userReviews: '',
   };
   emailFormControl = new FormControl('', [
     Validators.required,
@@ -19,25 +38,38 @@ export class LoginPageComponent implements OnInit {
   ]);
 
   login: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required ]),
-    password: new FormControl('', [Validators.required, Validators.min(3) ])
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.min(3)]),
   });
   hide = true;
-  get emailInput() { return this.login.get('email'); }
-  get passwordInput() { return this.login.get('password'); }
-
-  constructor() { }
-
-  ngOnInit(): void {
-
+  get emailInput() {
+    return this.login.get('email');
   }
+  get passwordInput() {
+    return this.login.get('password');
+  }
+
+  constructor(private loginService: LoginService, private router: Router) {}
+
+  ngOnInit(): void {}
   logIn(): void {
     this.user.email = this.emailInput.value;
     this.user.password = this.passwordInput.value;
-    console.log('email', this.user.email);
 
+    this.loginService.getUsers().subscribe((user) => {
+      console.log(user);
+      user.forEach((element) => {
+        if (
+          element.email === this.user.email &&
+          element.password === this.user.password
+        ) {
+          console.log('sve se poklapa');
+          this.router.navigate(['/home-page']);
+        } else {
+          document.getElementById("wrongData").innerHTML = 'Wrong Credentials. Invalid username or password';
+
+        }
+      });
+    });
   }
-
-
-
 }
